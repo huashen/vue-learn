b7a0538363e37fd9d1938a63534b7eef6ba2396f
huashen <huashen361@gmail.com>
2017-03-08 00:03:34 +0800

    Vue组件学习

---
+++ components/main.js
@@ -0,0 +1,29 @@
+/**
+ * Created by longhuasshen on 17/3/7.
+ */
+Vue.component('task-list', {
+       template: `
+            <div>
+                <task v-for="task in tasks">{{ task.task }}</task>
+            </div>
+        `,
+        data() {
+            return {
+                tasks: [
+                    {task: 'Go to the store', complete: true},
+                    {task: 'Go to the email', complete: false},
+                    {task: 'Go to the farm', complete: true},
+                    {task: 'Go to the work', complete: false}
+                ]
+
+        };
+    }
+});
+
+Vue.component('task', {
+    template: '<li><slot></slot></li>'
+});
+
+new Vue({
+    el: "#root"
+});
